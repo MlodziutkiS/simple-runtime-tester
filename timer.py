9d946b5039c9d112cbbd2f4ddd7d31983069b49b
MlodziutkiS <163459690+MlodziutkiS@users.noreply.github.com>
2024-07-01 21:02:53 +0200

    Update timer.py
    added parameter multiple program executions

--- timer.py
+++ timer.py
@@ -2,8 +2,14 @@ import sys
 import subprocess
 import time
 print("running test on", sys.argv[1])
+count=1
+try:
+    count=sys.argv[2]
+except:
+    print("run single execution")
 bef=time.time()
-cmd="python3 "+sys.argv[1]
-subprocess.Popen(cmd, shell=True).communicate(input=None)
+for x in range(0,int(count)):
+    cmd="python3 "+sys.argv[1]
+    subprocess.Popen(cmd, shell=True).communicate(input=None)
 now=time.time()
 print("time elapsed running",str(now - bef))
